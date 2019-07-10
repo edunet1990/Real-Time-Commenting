0e334c529c9e2d022e7b04c0094eccae0c43f1c1
Eduardo Estevão <eduardoestevaosilva@gmail.com>
2019-07-10 16:59:33 -0300

    First commit of one program thats I did to send comments in real time

---
+++ Real-Time-Commenting/Context/ApplicaitonDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Real_Time_Commenting.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Real_Time_Commenting.Context  
+{
+    public class ApplicaitonDbContext : DbContext
+    {
+        public ApplicaitonDbContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<BlogPost> BlogPost { get; set; }
+        public DbSet<Comment> Comment { get; set; }
+    }
+}
